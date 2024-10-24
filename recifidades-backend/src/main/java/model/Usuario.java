80ce337316bdf36cb89907b2d7cd722297e59ca3
Vitórya Medeiros <graziellevitorya05@gmail.com>
2024-10-24 11:54:58 -0300

    Add files via upload

---
+++ recifidades-backend/src/main/java/model/Usuario.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Usuario {
+    private int idUsuario;
+    private String nome;
+    private String email;
+    private String senha;
+    private int pontosAcumulados;
+    private int saldoMoedasCapibas;
+
+    private List<PontoTuristico> locaisVisitados;
+
+}
